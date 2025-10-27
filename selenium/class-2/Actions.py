c8afe0aab81811adec2cd8bdb7411cbb32c99d8e
hiromia006 <hiromia006@gmail.com>
2025-10-27 21:56:41 +0600

    class class

--- selenium/class-2/Actions.py
+++ selenium/class-2/Actions.py
@@ -11,11 +11,22 @@ service = Service("/snap/bin/geckodriver")
 
 driver = webdriver.Firefox(service=service)
 driver.get("https://www.tutorialspoint.com/selenium/practice/droppable.php")
-wait = WebDriverWait(driver, 10)
+driver.implicitly_wait(10)
 driver.maximize_window()
 
 source = driver.find_element(By.ID, "draggable")
 target = driver.find_element(By.ID, "droppable")
 ActionChains(driver).drag_and_drop(source, target).perform()
 time.sleep(3)
+
+driver.get("https://www.tutorialspoint.com/selenium/practice/buttons.php")
+
+button = driver.find_element(By.CSS_SELECTOR, ".btn.btn-secondary");
+ActionChains(driver).context_click(button).perform()
+time.sleep(2)
+driver.find_element(By.TAG_NAME, "body").click()  # Click elsewhere to close context menu
+time.sleep(2)
+
+
+
 driver.quit()
